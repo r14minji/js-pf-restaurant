554b21dca8cbefde09ab77ff0a0fdcad021cf685
r14minji <rkdalswl0728@gmail.com>
2022-01-26 23:58:24 +0900

    버튼 적용_색상변경 수정필요

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,4 +1,29 @@
+import { useEffect } from "react";
+
 function Header(){
+
+  useEffect(()=>{
+    var boxes = document.querySelectorAll('#boxes > div');
+  [].forEach.call(boxes, box => {
+    box.addEventListener('mousemove', e => {
+      document.body.style.setProperty(
+        'backgroundColor',
+        box.style.getPropertyValue('color')
+      );
+
+      var size = parseInt(getComputedStyle(box).width);
+      
+      // scaling
+      var x = size * .3 * .7 + .7 * e.offsetX;
+      var y = size * .3 * .7 + .7 * e.offsetY;
+      
+      box.style.setProperty('--x', x);
+      box.style.setProperty('--y', y);
+      box.style.setProperty('--size', size);
+    });
+  });
+  },[])
+
   return (
     <>
       <h1>
@@ -7,11 +32,11 @@ function Header(){
       </h1>
 
       <nav id="gnb">
-        <ul>
-          <li>Seoul</li>
-          <li>Jeju</li>
-          <li>Gyeongju</li>
-        </ul>
+        <div id="boxes">
+          <div style={{color: "#f44336"}}>Seoul</div>
+          <div style={{color: "#e91e63"}}>Jeju</div>
+          <div style={{color: "#9c27b0"}}>Gyeongju</div>
+        </div>
       </nav>
 
     </>
